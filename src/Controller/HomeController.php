1e32582c18980c645d8ee5b34dc004a425420aff
virginie lebleu <virginie.lebleu59@gmail.com>
2023-06-13 17:25:48 +0200

    caroussel connecter à la bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\PaintingRepository;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'app_home')]
+
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
-    }
-}
+            #[Route('/', name: 'index')]
+
+        public function index(PaintingRepository $paintingRepository): Response
+{
+            $paintings = $paintingRepository->findBy([], null, 4);
+
+            return $this->render('home/index.html.twig', [
+                'paintings' => $paintings,
+            ]);
+        }
+        }
+   
+
