24ee4406cc4d1b3cc689216c08676ce52fc85326
johann gonin <[MY\_johann.g.37@gmail.com](mailto:johann.g.37@gmail.com)>
2023-06-14 17:34:39 +0200

    add paginator

--- src/Controller/GaleryController.php
+++ src/Controller/GaleryController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\PaintingRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,12 +13,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class GaleryController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(PaintingRepository $paintingRepositery): Response
-    {
-        $paintings = $paintingRepositery->findBy(criteria:[], limit:6);
-
+    public function index(
+        PaintingRepository $paintingRepositery,
+        PaginatorInterface $paginator,
+        Request $request
+        ): Response {
+        $pagination = $paginator->paginate(
+            $paintingRepositery->queryFindAll(),
+            $request->query->getInt('page', 1),6
+        );
         return $this->render('galery/index.html.twig', [
-            'paintings' => $paintings,
+            'paintings' => $pagination,
         ]);
+
     }
 }
