10e2de4d42bd2ee9132798b2443e00366037cc2d
valphi41 <philippeau.valentin@gmail.com>
2023-06-22 14:56:56 +0200

    Ajout du crud user et ajout de la classe table-responsive pour les artistes

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -53,7 +53,7 @@ class AdminUserController extends AbstractController
     #[Route('/{id}', name: 'app_admin_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, UserRepository $userRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $userRepository->remove($user, true);
         }
 
