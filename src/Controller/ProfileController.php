392f1ee80e24b566995289f309a2f72997331a24
virginie lebleu <virginie.lebleu59@gmail.com>
2023-06-26 11:35:11 +0200

    ajout page profile+edit

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/profile', name: 'app_profile_')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+
+    #[Route('/', name: 'index')]
+     public function index(): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
-        ]);
+        return $this->render('profile/index.html.twig');
+    }
+
+    #[Route('/edit', name: 'edit')]
+    public function edit(): Response
+    {
+        return $this->render('profile/edit.html.twig');
     }
 }
