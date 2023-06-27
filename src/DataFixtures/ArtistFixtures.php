96e50cb972f7ac66cd91f0378c0febace26e141d
valphi41 <philippeau.valentin@gmail.com>
2023-06-27 12:15:16 +0200

    save work

--- src/DataFixtures/ArtistFixtures.php
+++ src/DataFixtures/ArtistFixtures.php
@@ -15,7 +15,7 @@ class ArtistFixtures extends Fixture
         $artist = new Artist();
         $artist->setName('Hypolite Charles Napoleon Mortier');
         $artist->setDateofBirth($faker->dateTime());
-        $artist->setNationality('French');
+        $artist->setNationality('Français.e');
         $artist->setBiography($faker->paragraph(7));
         $this->addReference('artist_1', $artist);
         $manager->persist($artist);
@@ -23,7 +23,7 @@ class ArtistFixtures extends Fixture
             $artist = new Artist();
             $artist->setName($faker->name());
             $artist->setDateofBirth($faker->dateTime());
-            $artist->setNationality('French');
+            $artist->setNationality('Français.e');
             $artist->setBiography($faker->paragraph(7));
             $this->addReference('artist_' . ($i % 7 + 1), $artist);
             $manager->persist($artist);
