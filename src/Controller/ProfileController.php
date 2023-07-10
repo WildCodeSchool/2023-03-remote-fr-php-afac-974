20796ce9d9bbf37a4afbf677baad3d27ef7cb472
virginie lebleu <virginie.lebleu59@gmail.com>
2023-07-10 11:11:28 +0200

    qualité du code

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,20 +7,26 @@ use App\Form\ProfileType;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Painting;
+use App\Repository\PaintingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 #[Route('/profile', name: 'app_profile_')]
 class ProfileController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    
-    public function index(): Response
-    {
 
+    #[Route('/profile', name: 'app_profile')]
+    public function index( Painting $paintings, user $user): Response
+    {
         return $this->render(
             'profile/index.html.twig',
+            [
+                'paintings' => $paintings,
+                'user' => $user
+            ]
         );
     }
 }
