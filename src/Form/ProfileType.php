c6f61ab38547bd8b0d97f759f6b0ca1217b8e1f8
virginie lebleu <virginie.lebleu59@gmail.com>
2023-07-10 15:19:14 +0200

    modif

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -18,20 +18,9 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class, ['label' => 'Prénom'])
-            ->add('lastname', TextType::class, ['label' => 'Nom'])
-            ->add('email', EmailType::class)
-            ->add('password', RepeatedType::class, array(
-                    'type' => PasswordType::class,
-                    'first_options' => ['label' => 'Nouveau mot de passe'],
-                    'second_options' => ['label' => 'Confirmer nouveau mot de passe'],
-                    'invalid_message' => 'Les deux mots de passe doivent être identiques',
-                    'options' => array(
-                        'attr' => array(
-                            'class' => 'password-field'
-                        )
-                    ),
-                    'required' => true,
-                ));
-    }
+        ->add('firstname', TextType::class, ['label' => 'Prénom'])
+        ->add('lastname', TextType::class, ['label' => 'Nom'])
+        ->add('email', EmailType::class);
 }
+}
+
