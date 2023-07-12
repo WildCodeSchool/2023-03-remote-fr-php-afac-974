a4d9d6e147aa3273c83f1dae3958cc7c9113b572
Victoria GAMBIER <gambiervictoria@hotmail.fr>
2023-07-12 11:14:43 +0200

    Ajout étoiles et liens

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/profile', name: 'app_profile_')]
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
